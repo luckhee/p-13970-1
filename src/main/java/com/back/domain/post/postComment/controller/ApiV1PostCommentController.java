48cb342b9cda728e58aa4d947e26e294dd02a809
luckhee <dcs1206@naver.com>
2025-06-26 11:28:33 +0900

    postman

--- src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
+++ src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
@@ -3,14 +3,11 @@ package com.back.domain.post.postComment.controller;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import com.back.domain.post.postComment.dto.PostCommentDto;
-import com.back.global.rsData.RsData;
 import com.back.domain.post.postComment.entity.PostComment;
+import com.back.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -42,9 +39,9 @@ public class ApiV1PostCommentController {
 
         return new PostCommentDto(postComment);
     }
-    @GetMapping("/{id}/delete")
+    @DeleteMapping("/{id}")
     @Transactional
-    public RsData delete(@PathVariable int id , @PathVariable int postId) {
+    public RsData<Void> delete(@PathVariable int id , @PathVariable int postId) {
         Post post = postService.findById(postId).get();
 
 
@@ -53,7 +50,7 @@ public class ApiV1PostCommentController {
 
         postService.deleteComment(post, postComment);
 
-        return new RsData("200-1", "댓글이 삭제되었습니다.", new PostCommentDto(postComment));
+        return new RsData<>("200-1", "댓글이 삭제되었습니다.");
     }
 
 }
